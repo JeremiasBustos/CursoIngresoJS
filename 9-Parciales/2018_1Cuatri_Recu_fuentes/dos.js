dde3d34c2fc324f11549b0d0abcd9f6083db6550
alumno <alumno>
2019-02-15 09:47:37 -0300

    Termine los dos ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
-  
+  var compra;
+  var compraConDescuento;
+  var descuento;
+  var iva;
+  var precioFinal;
+
+  compra=document.getElementById("elNombre").value;
+
+  compra=parseInt(compra)
+
+  descuento=compra * .10
+
+  compraConDescuento=compra - descuento;
+
+  iva=compraConDescuento * .21;
+
+  precioFinal=compraConDescuento + iva;
+
+  alert("Tu compra es de $" + compra +  ", tenes un descuento del 10% queda en $" + compraConDescuento  + " mas el IVA es $ " + precioFinal)
+
+
 }
