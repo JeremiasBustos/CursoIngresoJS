5169e0d8070e0207cb53973659e4b37a22c6a193
alumno <alumno>
2019-02-15 10:22:06 -0300

    Termine el ejercicio 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
 
+var precio;
+var propina;
+var precioMasPropina;
+var iva;
+var precioTotal;
+
+precio=prompt("Ingresar precio");
+
+precio=parseInt(precio);
+
+propina=precio * .10
+
+precioMasPropina=precio + propina;
+
+iva=precioMasPropina * .21
+
+precioTotal=precioMasPropina - iva;
+
+alert("El precio es " + precio + ", mas la propina, el precio es " + precioMasPropina + " el precio con la propina y sin IVA es " + precioTotal );
+
+
 }
