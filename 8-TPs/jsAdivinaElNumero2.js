1ea848858e87f06e893a438bd4f5e128bad8fcbb
alumno <alumno>
2019-02-21 12:30:49 -0300

    Mas ejerciciobs completados del TP

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -16,13 +16,51 @@ function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
-	
+	numeroSecreto=Math.floor(Math.random() * 101);
+	console.log("El numero es " + numeroSecreto);
+	contadorIntentos=0
 
 }
 
 function verificar()
 {
-	
-	
+	var numeroIngresado;
+	var mensaje;
+	contadorIntentos=contadorIntentos + 1;
+	numeroIngresado=numero.value;
+	numeroIngresado=parseInt(numeroIngresado);
+	console.log(numeroIngresado);
+	if(numeroIngresado==numeroSecreto)
+	{switch(contadorIntentos)
+		{
+			case 1:
+			alert("Usted es un Psíquico")
+			break
+			case 2:
+			alert("excelente percepción")
+			break
+			case 3:
+			alert("Eso es suerte")
+			break
+			case 4:
+			alert("Excelente tecnica")
+			break
+			case 5:
+			alert("Usted esta en la media")
+			break
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			alert("falta tecnica")
+			break
+			default:
+			alert("afortunado en el amor!!");
+			break
+		}
+		intentos.value=contadorIntentos;
+	}
+
 
 }
